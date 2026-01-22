91f71519cd91428b5d50930b688b60305b0dffae
Eric Kunzel <kunzel.eric@gmail.com>
2026-01-22 14:49:44 -0300

    moved files out of app folder

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { database } from "@/app/infra/database";
+import { database } from "@/infra/database";
 
 export async function GET(request: NextRequest) {
   const updatedAt = new Date().toISOString();
